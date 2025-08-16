84c52a935dd38324c0245a5f53351bb2dcc23f56
Shivam Raj Gupta <guptashivam25oct@gmail.com>
2025-08-16 16:41:49 +0530

    first commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language, overview } }) => {
+	return (
+		<div className='movie-card'>
+			<img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'src/assets/no-movie.png'} alt={title} />
+
+			<div className='mt-4'>
+				<h3>{title}</h3>
+				<div className='content'>
+					<div className='rating'>
+						<img src="src/assets/star.svg" alt="" />
+						<p>{vote_average ? vote_average.toFixed(2) : 'N/A'}</p>
+					</div>
+
+					<span>&bull;</span>
+					<p className='lang'>{original_language ? original_language : 'N/A'}</p>
+					<span>&bull;</span>
+					<p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+				</div>
+
+			</div>
+		</div>
+	)
+}
+
+export default MovieCard
